7601657e49d3007829b515e194df7ff24607ae29
ebogdansan <bogdan.en03@gmail.com>
2026-05-01 18:42:51 +0300

    adaugat fisier biblioteca_tari.py si setari vscode

---
+++ app/lib/biblioteca_tari.py
@@ -0,0 +1,5 @@
+def populatie_norvegia ():
+    return print("Populația Norvegiei este de aproximativ 5,4 milioane de locuitori.")
+
+def capitala_norvegia ():
+    return print("Capitala Norvegiei este Oslo.")
